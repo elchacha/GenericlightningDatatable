d2c2c67410ff6e25b27f99c7b3d9f02a51d95d9d
elchacha <fabricechallier@gmail.com>
2024-06-21 15:38:21 +0200

    Update tableScreenFlowModal.js

--- genericTable/force-app/main/default/lwc/tableScreenFlowModal/tableScreenFlowModal.js
+++ genericTable/force-app/main/default/lwc/tableScreenFlowModal/tableScreenFlowModal.js
@@ -19,10 +19,14 @@ export default class TableScreenFlowModal extends LightningModal {
 
   // cette methodé renverra à l'appelant les variables "output" du screenflow
   handleStatusChange(event) {
+    debugger;
+    console.log('handleChange');
       if (event.detail.status === "FINISHED" || event.detail.status=="FINISHED_SCREEN") {
         let flowVariables={};
-        for (const flowVariable of event.detail.outputVariables) {  
-          flowVariables[flowVariable.name]=flowVariable.value;
+        if(event.detail&&event.detail.outputVariables){
+          for (const flowVariable of event.detail.outputVariables) {  
+            flowVariables[flowVariable.name]=flowVariable.value;
+          }
         }
         this.close({flowVariables});
     }
